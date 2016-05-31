9d5af7072df8ca077bbfefce4b607fd6f7f56d42
TeraBytesMemory <yuj.terazawa@gmail.com>
2016-05-31 20:44:30 +0900

    make flag option

--- src/command/command.py
+++ src/command/command.py
@@ -15,6 +15,37 @@ class Command(metaclass=ABCMeta):
         if self.data[0] != self.__class__.command():
             raise TypeError
 
+        self.flag = {}
+        self.default = {}
+
+    def _add_flag(self, callback, flag: str, default: str, full=None):
+        if flag[0] != '-':
+            raise ValueError
+        if flag[:1] == '--':
+            full = flag
+
+        self.flag[flag] = {
+            'callback': callback,
+            'default': default
+        }
+        self.default[flag] = default
+        if type(full) == str:
+            if flag[:1] != '--':
+                raise ValueError
+
+            self.flag[full] = {
+                'callback': callback,
+                'default': default
+            }
+
+    def _run_flag(self):
+        for k, v in self.flag:
+            if k in self.data:
+                i = self.data.index(k)
+                if i + 1 == len(self.data):
+                    v['callback'](v['default'])
+
+
     @abstractmethod
     def run(self):
         pass
